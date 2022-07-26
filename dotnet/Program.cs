ec1e6c56dc9cc5fcee608fc0f58d3f1c4d1be3fc
Chris Paton <chrispaton2005@gmail.com>
2022-07-26 16:18:54 +1000

    fix: stuff

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -7,7 +7,6 @@ namespace dotnet
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
         }
     }
 }
